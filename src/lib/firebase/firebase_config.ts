cbc7f761b77db4ad796a37bcc4d26566a241b724
Mohamed Abdinasir <abdinasirm467@gmail.com>
2025-08-15 21:27:22 +0000

    The `npm install` command failed in my project. Analyze the following er

--- src/lib/firebase/firebase_config.ts
+++ src/lib/firebase/firebase_config.ts
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC87QSxzIcatmngHU1n3u9JVnHq5JAbjME",
-  authDomain: "cultered-nomads.firebaseapp.com",
-  projectId: "cultered-nomads",
-  storageBucket: "cultered-nomads.appspot.com",
-  messagingSenderId: "422626062362",
-  appId: "1:422626062362:web:76b01c2b2c5a47ed78a1a3",
-  measurementId: "G-FG5F9XMR62"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
