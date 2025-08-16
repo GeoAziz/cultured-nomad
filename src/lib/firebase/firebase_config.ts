e0f79fe3718812d8aff98ac8164c927bee74fa7a
Mohamed Abdinasir <abdinasirm467@gmail.com>
2025-08-16 10:42:28 +0000

    Error: Login error from callback: "Email/Password sign-in is not enabled

--- src/lib/firebase/firebase_config.ts
+++ src/lib/firebase/firebase_config.ts
@@ -1,19 +1,18 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, setPersistence, browserLocalPersistence, type Auth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration is loaded from environment variables
+// THIS IS INTENTIONALLY HARDCODED TO FIX A PERSISTENT CONFIGURATION BUG
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  // Conditionally add measurementId only if it exists
-  ...(process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID && { measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID }),
+    "apiKey": "AIzaSyC3PF1EbW-a9Gv_N7sT_bhLORt3Uog31KM",
+    "authDomain": "cultured-nomads-44cec.firebaseapp.com",
+    "projectId": "cultured-nomads-44cec",
+    "storageBucket": "cultured-nomads-44cec.appspot.com",
+    "messagingSenderId": "1006509653860",
+    "appId": "1:1006509653860:web:9c5a7516d2a762b7194483",
+    "measurementId": "G-5B995J351J"
 };
 
 // Initialize Firebase
