b598da5d572756e3a645c8807944b16a11301700
Mohamed Abdinasir <abdinasirm467@gmail.com>
2025-08-18 05:34:47 +0000

    Download the React DevTools for a better development experience: https:/

--- functions/lib/functions/src/index.js
+++ functions/lib/functions/src/index.js
@@ -15,23 +15,13 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getDailyPrompt = exports.getMentorDashboardStats = exports.createBroadcast = exports.publishStory = exports.logMood = exports.requestMentorship = exports.sendMessage = exports.rsvpToEvent = exports.updateUserProfile = exports.onUserDelete = exports.assignUserRole = void 0;
 const admin = __importStar(require("firebase-admin"));
@@ -266,21 +256,6 @@ exports.createBroadcast = functions.https.onCall(async (data, context) => {
  */
 var getMentorDashboardStats_1 = require("./getMentorDashboardStats");
 Object.defineProperty(exports, "getMentorDashboardStats", { enumerable: true, get: function () { return getMentorDashboardStats_1.getMentorDashboardStats; } });
-// Other existing exports and functions...
-throw new functions.https.HttpsError("unauthenticated", "You must be logged in.");
-const mentorId = context.auth.uid;
-const mentorshipsRef = db.collection("mentorships");
-const pendingQuery = mentorshipsRef.where('mentorId', '==', mentorId).where('status', '==', 'pending');
-const acceptedQuery = mentorshipsRef.where('mentorId', '==', mentorId).where('status', '==', 'accepted');
-const [pendingSnapshot, acceptedSnapshot] = await Promise.all([
-    pendingQuery.get(),
-    acceptedQuery.get()
-]);
-return {
-    pendingRequests: pendingSnapshot.size,
-    activeMentees: acceptedSnapshot.size,
-};
-;
 /**
  * Helper function to push a notification to a user's subcollection.
  */
